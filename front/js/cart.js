61abfec5931c7dfb66c14ffa84682577e660eb91
Elhem <elhemmansour@hotmail.fr>
2022-08-27 15:30:12 +0200

    cart page is ok

--- front/js/cart.js
+++ front/js/cart.js
@@ -13,23 +13,53 @@ function displayItem(item){
     const article = makeArticle (item)
     const imageDiv = makeImageDiv (item)
     article.appendChild (imageDiv)
+  const cartItemContent = makeCartContent(item) 
+  article.appendChild(cartItemContent)
+  displayArticle(article)
 
-  makeCartContent(item)
-   const cartItemContent = makeCartContent(item)
-   article.appendChild(cartItemContent) 
-   displayArticle(article)
 }
 function makeCartContent (item){
     const cartItemContent = document.createElement("div")
-    const description = makeDescription(item)
-    const settings = makeSettings()
     cartItemContent.classList.add("cart_item_content")
+
+    const description = makeDescription(item)
+    const settings = makeSettings(item)
+ 
     cartItemContent.appendChild(description)
-   // cartItemContent.appendChild(settings)
+    cartItemContent.appendChild(settings)
     return cartItemContent
 }
 function makeSettings(item) {
-    return ""
+  const settings = document.createElement("div")
+  settings.classList.add("cart__item__content__settings")
+  addQuantityToSettings(settings, item);  
+  addDeleteToSettings(settings)
+  return settings 
+}
+function addDeleteToSettings(settings){
+  const div = document.createElement("div")
+  div.classList.add("cart__item__content__settings__delete")
+const p = document.createElement("p")
+p.textContent = "Suprimer"
+div.appendChild(p)
+settings.appendChild(div)
+}
+function addQuantityToSettings(settings, item){
+  const quantity = document.createElement("div")
+quantity.classList.add("cart__item__content__settings__quantity")
+const p = document.createElement("div")
+p.textContent = "Qté :"
+quantity.appendChild(p)
+const input = document.createElement("input")
+input.type = "number"
+input.classList.add("itemQuantity")
+input.name = "itemQuantity"
+input.min = "1"
+input.max = "100"
+input.value = item.quantity
+quantity.append(input)
+settings.appendChild(quantity)
+
 }
 function makeDescription(item){
     const description = document.createElement("div")
